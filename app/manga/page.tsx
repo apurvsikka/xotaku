99f548959aa059e9c24ba06db5b81e045ef09092
apurv <apurvsikka@gmail.com>
2025-08-27 23:36:57 +0530

    ---------

--- app/manga/page.tsx
+++ app/manga/page.tsx
@@ -60,7 +60,6 @@ export default function Home() {
   }, []);
   return (
     <>
-      <NavBar />
       <div className="lg:p-6 lg:pt-20 ">
         <MangaSlider />
         {/* <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div> */}
